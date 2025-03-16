047cbf9ed5e28056692b688b5efb1fdf1eae555e
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-03-17 02:27:56 +0530

    Update Firebase configuration for new project settings

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,38 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "realestate-6457e.firebaseapp.com",
+  projectId: "realestate-6457e",
+  storageBucket: "realestate-6457e.firebasestorage.app",
+  messagingSenderId: "619998974481",
+  appId: "1:619998974481:web:433490f565620f9ba2d7b0",
+  measurementId: "G-T6L22R4MFK"
 };
 
 // Initialize Firebase
