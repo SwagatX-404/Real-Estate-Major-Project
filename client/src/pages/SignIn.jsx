62281d42ea50c8f1011ef3813e321221b43898e1
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-04-09 20:25:16 +0530

    Enhance SignIn component styling with padding, border, and shadow adjustments

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -43,7 +43,7 @@ export default function SignIn() {
     }
   };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
+    <div className='p-6 max-w-lg mx-auto border-2 border-slate-500/50 rounded-xl shadow-md mt-20'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
@@ -63,7 +63,7 @@ export default function SignIn() {
 
         <button
           disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+          className='bg-slate-700 text-white p-3 rounded-full uppercase hover:opacity-95 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
